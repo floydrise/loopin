acd08617a4c31f9cf2ad2a6723a285584b628d38
Stefan <floydrise@gmail.com>
2025-05-12 19:07:49 +0300

    Add some changes
    Set up a proxy for vite so BE can communicate with FE,add theme switcher and a toggle

--- view/src/components/Header.tsx
+++ view/src/components/Header.tsx
@@ -1,11 +1,16 @@
 import {Link} from '@tanstack/react-router'
+import {ModeToggle} from "@/components/mode-toggle.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 export default function Header() {
     return (
-        <header className="p-2 flex gap-2 text-black justify-between">
-            <nav className="flex flex-row">
-                <div className="px-2 font-bold">
+        <header className="p-2 border gap-2">
+            <nav className="flex flex-row justify-start items-center gap-4">
+                <Button variant={"default"}>
                     <Link to="/">Home</Link>
+                </Button>
+                <div>
+                    <ModeToggle/>
                 </div>
             </nav>
         </header>
