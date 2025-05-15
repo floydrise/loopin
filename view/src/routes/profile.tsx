0e9871b4a8410e844bf0bc48bd0d7afb1ae76b1b
Stefan <floydrise@gmail.com>
2025-05-15 20:24:53 +0300

    Add parts to the /profile route

--- view/src/routes/profile.tsx
+++ view/src/routes/profile.tsx
@@ -1,9 +1,41 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { useSession } from "@/lib/auth_client.ts";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-export const Route = createFileRoute('/profile')({
+export const Route = createFileRoute("/profile")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/profile"!</div>
+  const { data } = useSession();
+  const user = data?.user;
+  return (
+    <div className={"md:mx-30"}>
+      <h1 className={"text-2xl font-bold ml-4"}>My profile:</h1>
+      <div className={"flex ml-8 items-center gap-1"}>
+        <Avatar className={"size-14 border border-black"}>
+          <AvatarImage src={user?.image ?? ""} alt="User avatar image" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+        <div>
+          <h1>{user?.name}</h1>
+          <p className={"text-gray-600 dark:text-gray-400 font-light text-sm"}>
+            {user?.email}
+          </p>
+          <p
+            className={"text-gray-600 font-semibold dark:text-gray-400 text-sm"}
+          >
+            Registered:{" "}
+            {user?.createdAt
+              .toISOString()
+              .split("T")[0]
+              .split("-")
+              .reverse()
+              .join("-")}
+          </p>
+        </div>
+      </div>
+      <h1 className={"text-2xl mt-10 font-bold ml-4"}>My orders:</h1>
+    </div>
+  );
 }
