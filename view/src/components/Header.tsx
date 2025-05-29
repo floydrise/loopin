9f250d633a0963e593b296e50b163f40ddb39ccb
Stefan <floydrise@gmail.com>
2025-05-29 16:55:10 +0300

    Add navbar animation on scroll

--- view/src/components/Header.tsx
+++ view/src/components/Header.tsx
@@ -6,17 +6,41 @@ import { Brush, House, LogIn, MapPin, Telescope } from "lucide-react";
 import { ProfileDropdown } from "@/components/ProfileDropdown.tsx";
 import { MenuDropdown } from "@/components/MenuDropdown.tsx";
 import { useMediaQuery } from "usehooks-ts";
+import { motion, useMotionValueEvent, useScroll } from "motion/react";
+import { useState } from "react";
 
 export default function Header() {
   const { pathname } = useLocation();
   const { data } = useSession();
   const isDesktop = useMediaQuery("(min-width: 768px)");
+  const [hidden, setHidden] = useState(false);
+  const { scrollY } = useScroll();
+
+  useMotionValueEvent(scrollY, "change", (latestValue) => {
+    const previous = scrollY.getPrevious();
+    if (latestValue > previous!) {
+      setHidden(true);
+    } else {
+      setHidden(false);
+    }
+  });
   return (
-    <header className="p-2 bg-background/50 backdrop-blur-sm gap-2 rounded-full border fixed top-4 w-fit left-1/2 -translate-x-1/2 z-50">
+    <motion.header
+      variants={{
+        visible: { y: 0 },
+        hidden: { y: "-140%" },
+      }}
+      animate={hidden ? "hidden" : "visible"}
+      transition={{
+        duration: 0.5,
+        delay: 0.2,
+      }}
+      className="p-2 bg-background/50 backdrop-blur-sm gap-2 rounded-full border fixed top-4 w-fit left-1/2 -translate-x-1/2 z-50"
+    >
       <nav className="flex flex-row justify-between items-center gap-4">
-        <Link to={"/"} className={"flex items-center gap-1"}>
+        <Link to={"/"} className={"flex items-center"}>
           <MapPin className={"size-8"} />
-          <h1 className={"font-cherry text-lg"}>LoopIn</h1>
+          <h1 className={"text-lg"}>LoopIn</h1>
         </Link>
         <div className={"flex gap-4 justify-center items-center"}>
           <div className={"md:flex gap-2 hidden items-center justify-center"}>
@@ -67,6 +91,6 @@ export default function Header() {
           </div>
         </div>
       </nav>
-    </header>
+    </motion.header>
   );
 }
